2579e093ba5de3e37099f475e74b3e8d0cd9350a
kichuu <krishnadevkichus@gmail.com>
2024-09-17 13:55:05 +0530

    added calculator ui and function

--- script.js
+++ script.js
@@ -1,65 +1,16 @@
-let n1 
-let n2
-let operator
-function add(n1 , n2){
-    return n1 + n2
+const inputbar = document.querySelector("#inputbar")
+function appendTohtml(input){
+    inputbar.value += input;
 }
 
-function subtract(n1 , n2){
-    return n1 - n2
+function equate() {
+    try {
+        inputbar.value = eval(inputbar.value)
+    } catch (error) {
+        inputbar.value = "error";
+    }
 }
 
-function multiply(n1 , n2){
-    return n1*n2
+function clearinput(){
+    inputbar.value = "";
 }
-
-function divide(n1 , n2){
-    return n1/n2
-}
-
-
-document.addEventListener('DOMContentLoaded', function(){
-    console.log("DOM Content Loaded");
-    let cbody = document.querySelector('.cal-body')
-    for(i=0;i<10;i++){
-
-
-const nums  = document.createElement("button")
-nums.classList.add("nums")
-nums.textContent = i
-nums.classList.add("num-" + i);
-
-cbody.appendChild(nums)
-}
-
-const plus  = document.createElement("button")
-plus.classList.add("plus")
-plus.textContent = "+"
-cbody.appendChild(plus)
-
-const minus  = document.createElement("button");
-minus.classList.add("minus");
-minus.textContent = "-";
-cbody.appendChild(minus);
-
-const multiplication = document.createElement("button");
-multiplication.classList.add("multiplication");
-multiplication.textContent = "x";
-cbody.appendChild(multiplication)
-
-const division = document.createElement("button");
-division.classList.add("division");
-division.textContent = "/";
-cbody.appendChild(division)
-
-const clearButton = document.createElement("button");
-clearButton.classList.add("clearb");
-clearButton.textContent = "Clear";
-cbody.appendChild(clearButton)
-
-const equalsButton = document.createElement("button");
-equalsButton.classList.add("equals");
-equalsButton.textContent = "=";
-cbody.appendChild(equalsButton);
-
-})
